814f478d7903ec7518f14c4b8a8322236bce8ebc
tushalbarvaliya <tushal.barvaliya@seaflux.tech>
2026-02-17 17:31:00 +0530

    style : new button animation on hover

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
-import { useState } from 'react';
-import React from 'react';
+import { useState } from "react";
+import React from "react";
 
-import NewChallenge from './NewChallenge.jsx';
-import { AnimatePresence } from 'framer-motion';
+import NewChallenge from "./NewChallenge.jsx";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function Header() {
   const [isCreatingNewChallenge, setIsCreatingNewChallenge] = useState();
@@ -17,15 +17,17 @@ export default function Header() {
 
   return (
     <>
-    <AnimatePresence>
-      {isCreatingNewChallenge && <NewChallenge onDone={handleDone} />}
-    </AnimatePresence>
+      <AnimatePresence>
+        {isCreatingNewChallenge && <NewChallenge onDone={handleDone} />}
+      </AnimatePresence>
 
       <header id="main-header">
         <h1>Your Challenges</h1>
-        <button onClick={handleStartAddNewChallenge} className="button">
+        <motion.button 
+        whileHover={{scale:1.1,transition:{type:'spring',stiffness:500}}}
+        onClick={handleStartAddNewChallenge} className="button">
           Add Challenge
-        </button>
+        </motion.button>
       </header>
     </>
   );

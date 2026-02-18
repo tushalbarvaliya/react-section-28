469e4ffae8d7c5d36ceaa41ed7aa07356d025809
tushalbarvaliya <tushal.barvaliya@seaflux.tech>
2026-02-18 10:00:50 +0530

    style:view details animated.

--- src/components/ChallengeItem.jsx
+++ src/components/ChallengeItem.jsx
@@ -2,7 +2,7 @@ import { useContext } from "react";
 import React from "react";
 
 import { ChallengesContext } from "../store/challenges-context.jsx";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function ChallengeItem({
   challenge,
@@ -29,7 +29,7 @@ export default function ChallengeItem({
   }
 
   return (
-    <motion.li layout exit={{opacity:0,y:-30}}>
+    <motion.li layout exit={{ opacity: 0, y: -30 }}>
       <article className="challenge-item">
         <header>
           <img {...challenge.image} />
@@ -58,14 +58,19 @@ export default function ChallengeItem({
               </motion.span>
             </button>
           </p>
-
-          {isExpanded && (
-            <div>
-              <p className="challenge-item-description">
-                {challenge.description}
-              </p>
-            </div>
-          )}
+          <AnimatePresence>
+            {isExpanded && (
+              <motion.div
+                initial={{ height: 0, opacity: 0 }}
+                animate={{ height: "auto", opacity: 1 }}
+                exit={{ opacity: 0, height: 0 }}
+              >
+                <p className="challenge-item-description">
+                  {challenge.description}
+                </p>
+              </motion.div>
+            )}
+          </AnimatePresence>
         </div>
       </article>
     </motion.li>
